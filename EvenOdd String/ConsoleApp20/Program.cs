7609fd5845f4f07c9fef43e23011f8d7493481e2
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-07-22 11:45:43 +0400

    EvenOdd String

---
+++ EvenOdd String/ConsoleApp20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "CodeWars";
+            string evenOutput = null;
+            string oddOutput = null;
+            for(int i =0;i<s.Length;i++)
+            {
+                if (i == 0 || i % 2 == 0)
+                    evenOutput += s[i].ToString();
+                else
+                    oddOutput += s[i].ToString();
+
+            }
+            
+            Console.Write($"{evenOutput}  {oddOutput}");
+
+        }
+    }
+}
