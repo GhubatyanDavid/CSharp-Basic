25650e704fb0aca25b42a09c46767de5f1909b1a
Ghubatyandavid <ghubatyandavid@gmail.com>
2021-08-01 14:38:25 +0400

    TwoFer

---
+++ TwoFer/TwoFer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TwoFer
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            /* Name	String to return Alice	One for Alice, one for me.
+                                     Bob	One for Bob, one for me.
+                                     One for you, one for me.*/
+            Console.WriteLine("Type your name please!");
+            while (true)
+            {
+                string name = Console.ReadLine();
+                TwoFer(name);
+                static void TwoFer(string name)
+                {
+                    Console.WriteLine($"One for {name}, one for me.");
+                }
+            }
+        }
+    }
+}
